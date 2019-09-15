d141137226b8bf64bfbd4c3176e0097d7fd46c1f
GLORIA <gloriagr8@gmail.com>
2019-09-15 16:34:08 +0300

    fixed the 'evaluate.py' sizes problem

--- code/evaluate.py
+++ code/evaluate.py
@@ -7,14 +7,14 @@ import librosa
 #### additional for loop to evaluate multiple songs #########
 # increase step to sample less points and decrease time
 sample_step = 10
-bass_gt_path = '../Recovered_Songs_bigger5/bass/050.wav'
-bass_rec_path = r'C:\Users\daisp\datasets\DSD100\Sources\Test\050 - Zeno - Signs\bass.wav'
-vocal_gt_path = '../Recovered_Songs_bigger5/vocals/050.wav'
-vocal_rec_path = r'C:\Users\daisp\datasets\DSD100\Sources\Test\050 - Zeno - Signs\vocals.wav'
-drums_gt_path = '../Recovered_Songs_bigger5/drums/050.wav'
-drums_rec_path = r'C:\Users\daisp\datasets\DSD100\Sources\Test\050 - Zeno - Signs\drums.wav'
-others_gt_path = '../Recovered_Songs_bigger5/others/050.wav'
-others_rec_path = r'C:\Users\daisp\datasets\DSD100\Sources\Test\050 - Zeno - Signs\other.wav'
+bass_rec_path = '../Recovered_Songs_bigger5/bass/001.wav'
+bass_gt_path = '../../adversarial-audio-separation/AdversarialAudioSeparation/Data/DSD100/DSD100/Sources/Test/001 - ANiMAL - Clinic A/bass.wav'
+vocal_rec_path = '../Recovered_Songs_bigger5/vocals/001.wav'
+vocal_gt_path = '../../adversarial-audio-separation/AdversarialAudioSeparation/Data/DSD100/DSD100/Sources/Test/001 - ANiMAL - Clinic A/vocals.wav'
+drums_rec_path = '../Recovered_Songs_bigger5/drums/001.wav'
+drums_gt_path = '../../adversarial-audio-separation/AdversarialAudioSeparation/Data/DSD100/DSD100/Sources/Test/001 - ANiMAL - Clinic A/drums.wav'
+others_rec_path = '../Recovered_Songs_bigger5/others/001.wav'
+others_gt_path = '../../adversarial-audio-separation/AdversarialAudioSeparation/Data/DSD100/DSD100/Sources/Test/001 - ANiMAL - Clinic A/other.wav'
 ############################################################
 
 sample_rate, offset, duration = 44100, 30 * 0.3, 170 * 0.3
@@ -26,24 +26,36 @@ for gt_path, rec_path in zip(gt_instruments_paths, rec_instruments_paths):
     gt_tracks.append(librosa.load(gt_path, sr=sample_rate, offset=offset, duration=duration)[0])
     rec_tracks.append(librosa.load(rec_path, sr=sample_rate)[0])
 
-for gt_track, rec_track in zip(gt_tracks, rec_tracks):
-    # gt_track and rec_track are 2-element tuples whose first element contains the actual samples
-    # and the second element is the sample rate
-    gt_track = gt_track[0:rec_track.shape[0]:sample_step]
-    gt_track = np.transpose(gt_track.reshape(len(gt_track), 1))
+# gt_track and rec_track are 2-element tuples whose first element contains the actual samples
+# and the second element is the sample rate
+length = rec_tracks[0].shape[0]
+
+gt_tracks[:] = [gt_track[0:length:sample_step] for gt_track in gt_tracks]
+len_gt = gt_tracks[0].shape[0]
+gt_tracks[:] = [np.transpose(gt_track.reshape(len_gt, 1)) for gt_track in gt_tracks]
+
+rec_tracks[:] = [rec_track[0:length:sample_step]for rec_track in rec_tracks]
+len_rec = rec_tracks[0].shape[0]
+rec_tracks[:] = [np.transpose(rec_track.reshape(len_rec, 1)) for rec_track in rec_tracks]
+
+# for gt_track, rec_track in zip(gt_tracks, rec_tracks):
+#     gt_track = gt_track[0:rec_track.shape[0]:sample_step]
+#     gt_track = np.transpose(gt_track.reshape(len(gt_track), 1))
 
 final_gt = np.concatenate(gt_tracks, axis=0)
 print("final_gt shape = " + str(final_gt.shape))
 
-for rec_track in rec_tracks:
-    # rec_track is a 2-element tuple whose first element contains the actual samples
-    # and the second element is the sample rate
-    rec_track = rec_track[0:rec_track.shape[0]:sample_step]
-    rec_track = np.transpose(rec_track.reshape(len(rec_track), 1))
+# for rec_track in rec_tracks:
+#     rec_track = rec_track[0:rec_track.shape[0]:sample_step]
+#     rec_track = np.transpose(rec_track.reshape(len(rec_track), 1))
 
 final_rec = np.concatenate(rec_tracks, axis=0)
-print("final_gt shape = " + str(final_rec.shape))
+print("final_rec shape = " + str(final_rec.shape))
 
+# % [SDR,SIR,SAR,perm]=bss_eval_sources(se,s)
+# % Inputs:
+# % se: nsrc x nsampl matrix containing estimated sources
+# % s: nsrc x nsampl matrix containing true sources
 SDR, SIR, SAR, perm = mir_eval.separation.bss_eval_sources(final_rec, final_gt)
 
 print(SDR)
